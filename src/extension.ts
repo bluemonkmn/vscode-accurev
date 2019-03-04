2c85f343a87130923dfa7a38dccbf2dcfa206281
Ben Marty <ben.marty@infor.com>
2019-03-04 12:46:18 -0600

    Clean up activation and deactivation

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,58 @@
 import * as vscode from 'vscode';
 import { AccuRevRepo } from './repository';
 
+let globalState: {context: vscode.ExtensionContext,
+	config: vscode.WorkspaceConfiguration, 
+	channel: vscode.OutputChannel,
+	disposables: vscode.Disposable[],
+	configListener: vscode.Disposable | null,
+	dispose: () => void} | null;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Extension "AccuRev" is now active.');
+	console.log('AccuRev extension is listening for configuration changes.');
+
+	let config = vscode.workspace.getConfiguration("accurev");
+
+	globalState = {context: context,
+		config: config,
+		channel: vscode.window.createOutputChannel("AccuRev Log"),
+		disposables: [],
+		configListener: null,
+		dispose: () => {deactivate();}};
+
+	globalState.configListener = vscode.workspace.onDidChangeConfiguration((section) => {
+		if (section.affectsConfiguration("accurev.enabled")) {
+			if (globalState === null) {
+				return;
+			}
+			globalState.config = vscode.workspace.getConfiguration("accurev");
+			if (globalState.disposables.length === 0) { // enabled setting isn't updated yet.
+				innerActivate(context);
+			} else {
+				innerDeactivate();
+			}
+		}
+	});
+
+	if (config.enabled) {
+		innerActivate(context);
+	} else {
+		globalState.channel.appendLine("AccuRev extension is inactive except to listen for activation via enabled setting.");
+	}
+
+	context.subscriptions.push(globalState);
+}
+
+function innerActivate(context: vscode.ExtensionContext) {
+	if (globalState === null) {
+		return;
+	}
+
 	let folder: string = vscode.env.appRoot;
 	let scm: vscode.SourceControl | undefined;
 	if (vscode.workspace.workspaceFolders) {
@@ -18,28 +63,25 @@ export function activate(context: vscode.ExtensionContext) {
 		folder = rootUri.fsPath;
 	}
 
-	const repo = new AccuRevRepo(getOutputChannel(), folder);
+	const repo = new AccuRevRepo(globalState.channel, folder, globalState.config);
 	if (scm) {
+		globalState.disposables.push(scm, repo);
 		scm.quickDiffProvider = repo;
 		let modified = scm.createResourceGroup("modified", "Modified");
+		globalState.disposables.push(modified);
 		repo.getResourceStates().then((result) => {
 			modified.resourceStates = result;
 		});
-		context.subscriptions.push(modified);
 	}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('accurev.refresh', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		getOutputChannel().appendLine('Hello World!');
+	globalState.disposables.push(vscode.commands.registerCommand('accurev.refresh', () => {
+		if (globalState !== null) {
+			globalState.channel.appendLine('Hello World!');
+		}
 		repo.getPending();
-	});
+	}));
 
-	let diff = vscode.commands.registerCommand('accurev.openDiffBasis', async (file: vscode.Uri) => {
+	globalState.disposables.push(vscode.commands.registerCommand('accurev.openDiffBasis', async (file: vscode.Uri) => {
 		try {
 			let original = await repo.provideOriginalResource(file);
 			if (original !== null) {
@@ -48,20 +90,33 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		}
 		catch(err) {
-			getOutputChannel().appendLine(err);
+			if (globalState) {
+				globalState.channel.appendLine(err);
+			}
 		}
-	});
+	}));
 
-	context.subscriptions.push(disposable, repo, diff);
-}
-
-let channel: vscode.OutputChannel;
-function getOutputChannel(): vscode.OutputChannel {
-	if (!channel) {
-		channel = vscode.window.createOutputChannel("AccuRev Log");
-	}	
-	return channel;
+	globalState.channel.appendLine("AccuRev extension is now active.");
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (globalState === null) {
+		return;
+	}
+	if (globalState.configListener !== null) {
+		globalState.configListener.dispose();
+		globalState.configListener = null;
+	}
+}
+
+function innerDeactivate() {
+	if (globalState === null) {
+		return;
+	}
+	globalState.disposables.forEach(disposable => {
+		disposable.dispose();
+	});
+	globalState.disposables = [];
+	globalState.channel.appendLine("AccuRev extension deactivated.");
+}
--- src/repository.ts
+++ src/repository.ts
@@ -5,19 +5,22 @@ import { promisify } from 'util';
 import { AccuRevFile } from './file';
 import { AccuRevState } from './state';
 import * as path from 'path';
+import { configure } from 'vscode/lib/testrunner';
 
 export class AccuRevRepo {
     outChannel: vscode.OutputChannel;
     workspaceRoot: string;
     workspaceName: string;
     basisName: string;
-    disposables: vscode.Disposable[] = [];
+    disposables: Map<string, vscode.Disposable> = new Map<string, vscode.Disposable>();
+    config: vscode.WorkspaceConfiguration;
 
-    public constructor(out: vscode.OutputChannel, workspaceRoot: string) {
+    public constructor(out: vscode.OutputChannel, workspaceRoot: string, config: vscode.WorkspaceConfiguration) {
         this.outChannel = out;
         this.workspaceRoot = workspaceRoot;
         this.workspaceName = "";
         this.basisName = "";
+        this.config = config;
         this.getInfo();
     }
 
@@ -62,10 +65,18 @@ export class AccuRevRepo {
     }
 
     public async provideOriginalResource(uri: vscode.Uri): Promise<vscode.Uri | null> {
-        let originalText = await this.execute(`cat -v ${this.basisName} \"${uri.fsPath}\"`);
-        let tempDir = process.env["TEMP"];
+        let originalText: string;
+        try {
+            originalText = await this.execute(`cat -v ${this.basisName} \"${uri.fsPath}\"`);
+        }
+        catch(err) {
+            this.outChannel.appendLine(`Error retrieving original file: ${err}`);
+            return null;
+        }
+        await this.makeTempParentExist();
+        let tempDir = this.getTempDir();
         let tempExists = await new Promise<boolean>((resolve) => {
-            fs.access(`${tempDir}\\vscode-accurev`, (err) => {
+            fs.access(tempDir, (err) => {
                 if (err) {
                     resolve(false);
                 } else {
@@ -74,21 +85,59 @@ export class AccuRevRepo {
             });
         });
         if (!tempExists) {
-            let mkdir = promisify(fs.mkdir);
-            await mkdir(`${tempDir}\\vscode-accurev`);
+            let mkdir = promisify(fs.mkdir);            
+            await mkdir(tempDir, {recursive: true});
         }
         const filematch = /[\/\\]([^\/\\]+)$/.exec(uri.fsPath);
         if (filematch !== null) {
-            const tempPath = `${tempDir}\\vscode-accurev\\${filematch[1]}`;
+            const tempPath = `${tempDir}\\${filematch[1]}`;
             let writeFile = promisify(fs.writeFile);
             await writeFile(tempPath, originalText);
+            if (!this.disposables.has(tempPath)) {
+                this.disposables.set(tempPath, {dispose: async () => {
+                    let unlink = promisify(fs.unlink);
+                    try {
+                        await unlink(tempPath);
+                        try {
+                            this.outChannel.appendLine(`Cleaning up ${tempPath}...`);
+                        } catch(err2) { }
+                    } catch (err) {
+                        try {
+                            this.outChannel.appendLine(`Failed - ${err}.`);
+                        } catch(err3) { }
+                    }
+                }});
+            }
             return vscode.Uri.file(tempPath);
         }
         return null;
     }
 
     public async getResourceStates(): Promise<vscode.SourceControlResourceState[]> {
-        let resList = await this.execute("stat -p -fx");
+        let resList: string;
+        try {
+            resList = await this.execute("stat -p -fx");
+        }
+        catch (err) {
+            this.outChannel.appendLine(`Error retrieving pending file information: ${err}`);
+            if (/session token/.test(err)) {
+                if (!this.config.userid) {
+                    this.outChannel.appendLine("AccuRev User ID setting is not specified. To allow automatic login, enter this setting.");
+                    return [];
+                }
+                this.outChannel.appendLine("Attempting to login to resolve the problem...");
+                try {
+                   await this.execute(`login ${this.config.userid} ""`);
+                   resList = await this.execute("stat -p -fx");
+                } catch (err2) {
+                    this.outChannel.appendLine(`Failed to login: ${err2}`);
+                    return [];
+                }
+            } else {
+                this.outChannel.appendLine("Cause of error does not appear login related, giving up.");
+                return [];
+            }
+        }
         let match: RegExpExecArray | null;
         let result: vscode.SourceControlResourceState[] = [];
         let resourcePattern = /<element[^>]+\s+location=\"([^"\n]+)\"[^>]+\s+id=\"(\d+)\"[^>]+\s+status=\"([^"\n]+)\"/gm;
@@ -103,10 +152,55 @@ export class AccuRevRepo {
         return result;
     }
 
-    public dispose(): void {
-        this.disposables.forEach(disposable => {
-            disposable.dispose();
+    public dispose(): Promise<any> {
+
+        return new Promise(async (err) => {
+            this.disposables.forEach(disposable => {
+                disposable.dispose();
+            });
+            this.disposables.clear();
+            let rmdir = promisify(fs.rmdir);
+            let tempDir = this.getTempDir();
+            try {
+                await rmdir(tempDir);
+            } catch (err) {
+                try {
+                    this.outChannel.appendLine(`Error deleting ${tempDir}: ${err}.`);
+                } catch (err2) { }
+            }
         });
-        this.disposables = [];
+    }
+
+    private getTempDir(): string {
+        let tempDir = process.env["TEMP"];
+        if (tempDir === undefined) {
+            throw new Error("Unable to identify Temp directory.");
+        }
+        return path.join(tempDir, 'vscode-accurev', vscode.env.sessionId);
+    }
+
+    private async makeTempParentExist() {
+        let temp = process.env["TEMP"];
+        if (temp === undefined) {
+            throw new Error("Unable to identify Temp directory.");
+        }
+        let tempParent = path.join(temp, 'vscode-accurev');
+        let tempExists = await new Promise<boolean>((resolve) => {
+            fs.access(tempParent, (err) => {
+                if (err) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+        if (!tempExists) {
+            let mkdir = promisify(fs.mkdir);
+            try {
+                await mkdir(tempParent);
+            } catch (err) {
+                this.outChannel.appendLine(`Failed to create ${tempParent}: ${err}`);
+            }
+        }
     }
 }
